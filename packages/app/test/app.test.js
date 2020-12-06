450cb4cc6bf24b9828f7d66771e8e3e6b02f3e10
Hugo Pineda <hpineda83@gmail.com>
2020-12-06 09:52:15 -0800

    chore: added circleci config

---
+++ packages/app/test/app.test.js
@@ -0,0 +1,7 @@
+import App from '../src'
+
+describe('app', () => {
+  it('does not throw', () => {
+    expect(App).toEqual({})
+  })
+})
---
+++ packages/utils/test/sayHello.test.js
@@ -0,0 +1,7 @@
+const { sayHello } = require("../src")
+
+describe('sayHello', ()=>{
+  it('returns the greeting', ()=>{
+    expect(sayHello("World")).toEqual('Hello, World')
+  })
+})
