04eb39773795e0976edc771f4548472452742f4e
Hugo Pineda <hpineda83@gmail.com>
2020-12-06 10:23:05 -0800

    fix(utils): some changes

--- packages/utils/test/sayHello.test.js
+++ packages/utils/test/sayHello.test.js
@@ -2,6 +2,6 @@ const { sayHello } = require("../src")
 
 describe('sayHello', ()=>{
   it('returns the greeting', ()=>{
-    expect(sayHello("World")).toEqual('Hello, World')
+    expect(sayHello("World")).toEqual('Hello, World - 1')
   })
 })
